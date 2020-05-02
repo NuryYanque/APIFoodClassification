b368b75376317e3aad1c2c4a249a65b50418e711
Nury Yuleny Arosquipa Yanque <yuleny.aryan@gmail.com>
2020-05-02 12:56:01 -0300

    more readable code

--- run_keras_server.py
+++ run_keras_server.py
@@ -30,17 +30,17 @@ def load_all_models():
     g_model_objs = dict()
 
     g_model_objs = {
-        'model_nivel1': load_model_from_path('models/model_nivel1_VGG.h5'),
-        'model_nivel2_suco': load_model_from_path('models/model_nivel2_LeNetSuco.h5'),
+        'model_nivel1'             : load_model_from_path('models/model_nivel1_VGG.h5'),
+        'model_nivel2_suco'        : load_model_from_path('models/model_nivel2_LeNetSuco.h5'),
         'model_nivel2_refrigerante': load_model_from_path('models/model_nivel2_LeNetRefrigerante.h5'),
-        'model_nivel3_cocacola': load_model_from_path('models/model_nivel3_CocaCola.h5'),
-        'model_nivel3_guarana': load_model_from_path('models/model_nivel3_Guarana.h5'),
-        'model_nivel3_fanta': load_model_from_path('models/model_nivel3_Fanta.h5'),
-        'model_nivel3_pepsi': load_model_from_path('models/model_nivel3_Pepsi.h5'),
-        'model_nivel3_dobem': load_model_from_path('models/model_nivel3_DoBem.h5'),
-        'model_nivel3_delvalle': load_model_from_path('models/model_nivel3_DelValle.h5'),
-        'model_nivel3_maguary': load_model_from_path('models/model_nivel3_Maguary.h5'),
-        'model_nivel3_carrefour': load_model_from_path('models/model_nivel3_Carrefour.h5')
+        'model_nivel3_cocacola'    : load_model_from_path('models/model_nivel3_CocaCola.h5'),
+        'model_nivel3_guarana'     : load_model_from_path('models/model_nivel3_Guarana.h5'),
+        'model_nivel3_fanta'       : load_model_from_path('models/model_nivel3_Fanta.h5'),
+        'model_nivel3_pepsi'       : load_model_from_path('models/model_nivel3_Pepsi.h5'),
+        'model_nivel3_dobem'       : load_model_from_path('models/model_nivel3_DoBem.h5'),
+        'model_nivel3_delvalle'    : load_model_from_path('models/model_nivel3_DelValle.h5'),
+        'model_nivel3_maguary'     : load_model_from_path('models/model_nivel3_Maguary.h5'),
+        'model_nivel3_carrefour'   : load_model_from_path('models/model_nivel3_Carrefour.h5')
     }
 
 def processing_image(image, target):
@@ -62,40 +62,75 @@ def processing_image(image, target):
 
 # Asignação de nomes aos rótulos para cada classificador
 class_hierarchy = {
-    "produto": {0: "refrigerante", 1: "suco"},
-    "suco": {0: "carrefour", 1: "delValle", 2: "doBem", 3: "maguary" },
-    "refrigerante": {0: "coca-cola", 1: "fanta", 2: "guarana", 3: "pepsi"},
-    "doBem": {0: "limonada-doBem", 1: "manga-doBem", 2: "pessego-doBem", 3: "uva-doBem"},
-    "delValle": {0: "abacaxi-delValle", 1: "caju-delValle", 2: "laranja-delValle", 3: "maracuja-delValle", 4: "uva-delValle"},
-    "maguary": {0: "goiaba-maguary", 1: "manga-maguary", 2: "maracuja-maguary", 3: "morango-maguary", 4: "uva-maguary"},
-    "carrefour": {0: "abacaxi-carrefour", 1: "laranja-carrefour", 2: "pessego-maca-carrefour", 3: "uva-carrefour"},
-    "coca-cola": {0: "garrafa-original-cc", 1: "garrafa-semacucar-cc", 2: "lata-original-cc", 3: "lata-semacucar-cc"},
-    "fanta": {0: "garrafa-laranja-fanta", 1: "lata-guarana-fanta", 2: "lata-laranja-fanta"},
-    "guarana": {0: "garrafa-original-guarana", 1: "lata-antarctica-guarana", 2: "lata-original-guarana"},
-    "pepsi": {0: "garrafa-original-pepsi", 1: "garrafa-twist-pepsi", 2: "lata-original-pepsi", 3: "lata-semacucar-pepsi"},
+    "produto"     : { 0: "refrigerante", 
+                      1: "suco" 
+                    },
+    "suco"        : { 0: "carrefour", 
+                      1: "delValle", 
+                      2: "doBem", 
+                      3: "maguary" 
+                    },
+    "refrigerante": { 0: "coca-cola",
+                      1: "fanta", 
+                      2: "guarana", 
+                      3: "pepsi"
+                    },
+    "doBem"       : { 0: "limonada-doBem", 
+                      1: "manga-doBem", 
+                      2: "pessego-doBem", 
+                      3: "uva-doBem"
+                    },
+    "delValle"    : { 0: "abacaxi-delValle", 
+                      1: "caju-delValle", 
+                      2: "laranja-delValle", 
+                      3: "maracuja-delValle", 
+                      4: "uva-delValle"
+                    },
+    "maguary"     : { 0: "goiaba-maguary", 
+                      1: "manga-maguary", 
+                      2: "maracuja-maguary", 
+                      3: "morango-maguary", 
+                      4: "uva-maguary"
+                    },
+    "carrefour"   : { 0: "abacaxi-carrefour", 
+                      1: "laranja-carrefour", 
+                      2: "pessego-maca-carrefour", 
+                      3: "uva-carrefour"
+                    },
+    "coca-cola"   : { 0: "garrafa-original-cc", 
+                      1: "garrafa-semacucar-cc", 
+                      2: "lata-original-cc", 
+                      3: "lata-semacucar-cc"
+                    },
+    "fanta"       : { 0: "garrafa-laranja-fanta", 
+                      1: "lata-guarana-fanta", 
+                      2: "lata-laranja-fanta"
+                    },
+    "guarana"     : { 0: "garrafa-original-guarana", 
+                      1: "lata-antarctica-guarana", 
+                      2: "lata-original-guarana"
+                    },
+    "pepsi"       : { 0: "garrafa-original-pepsi", 
+                      1: "garrafa-twist-pepsi", 
+                      2: "lata-original-pepsi", 
+                      3: "lata-semacucar-pepsi"
+                    }
 }
 
 def prediction_by_model(image, graph, model):
-    print(image.shape)
     with graph.as_default():
-        print("fnjsrrgorghiarowsarjghaowefjoapefkaejaf")
         predictions = model.predict(image)
-        print("Prediction: ", predictions)
         return predictions
-    # except Exception as err:
-    #     raise(err)
-    # return None
 
 def run_predictions(image1, imagex):
     predictions = []
     # nivel 1
     graph, model_nivel1 = g_model_objs['model_nivel1']
-    #try:
+    
     produto = prediction_by_model(image1, graph, model_nivel1)
     y_predict_index = np.argmax(produto)
     predictions.append(class_hierarchy["produto"][y_predict_index])
-    # except Exception as error:
-    #     print("Error in predict")
+    
     #nivel 2
     if y_predict_index == 0: # refrigerante
         
@@ -203,35 +238,20 @@ def predict():
     image = Image.open(BytesIO(base64.b64decode(imageb64)))
     image = image.convert(mode='RGB')
     image = np.array(image)
-    print(np.array(image).shape)
+    
     image1, imagex = processing_image(image, target=(64,64))
-    print(np.array(image1).shape)
-    print(np.array(imagex).shape)
     # get detected product
     predictions = run_predictions(image1, imagex)
-
-    
-
-    # print(model.predict(image64x64))
-    # print(im)
-    # if flask.request.method == "POST":
-        # image = flask.request.form['image']
-        # print(image)
-        # if flask.request.files.get("image"):
-        #     image = flask.request.files["image"].read()
-        #     data["success"] = True
-        #     data["image"] = image
     
     data["success"] = True
     data["Prediction Nivel 1"] = predictions[0]
     data["Prediction Nivel 2"] = predictions[1]
     data["Prediction Nivel 3"] = predictions[2]
-    # K.clear_session()
+
     print(data)
     return flask.jsonify(data)
 
 
 if __name__ == "__main__":
-    # tf.initialize_all_variables().run()
     load_all_models()
     app.run()
